3a22753896c49783469764cdbc694b4f9d046207
minaa <mina.adel1121@gmail.com>
2024-05-06 16:34:53 +0300

    fix add route notifcaition

--- backend/Routes/notification.js
+++ backend/Routes/notification.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { notificationsController } from "../Controllers/notificationsController.js"
+import { authenticate } from "../auth/verifyToken.js"
 
 const router = express.Router()
 
-router.get('/', notificationsController.getNotification)
+router.get('/', authenticate, notificationsController.getNotification)
 router.post('/', notificationsController.addNotification)
 router.delete('/:id', notificationsController.deleteNotification)
 

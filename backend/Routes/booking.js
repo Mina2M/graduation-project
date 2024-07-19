2b06a363c5726b546cf47de9127632a56f136d37
Mina2M <104466755+Mina2M@users.noreply.github.com>
2024-07-19 13:31:18 +0300

    Update booking.js

--- backend/Routes/booking.js
+++ backend/Routes/booking.js
@@ -1,5 +1,4 @@
 import express from 'express'
-// import { getAllReview, createReview } from '../Controllers/reviewController.js'
 import { authenticate, restrict } from '../auth/verifyToken.js'
 import { getAllBooking, getStats, getEarningStats, getWeekSales, getTransactions, createStripe, updateBooking} from '../Controllers/bookingController.js';
 
@@ -12,9 +11,6 @@ router.get('/transaction', getTransactions)
 router.put('/:id', updateBooking)
 
 
-// // nesting route    
-// // doctor/DoctorId/bookings http://localhost:5000/api/v1/doctors/654e27f119b8075464c7ce89/bookings/
+// nesting route    
 router.route('/').get(getAllBooking).post(authenticate, restrict(['patient']), createStripe)
-// router.post('/:doctorId',authenticate, restrict(['patient']), createStripe)
-// router.get('/',authenticate, restrict(['patient']), getAllBooking)
 export default router;
